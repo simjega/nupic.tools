cf3ac97048c14e8ef147c4b984a9eb7f89112a12
rhyolight <rhyolight@gmail.com>
2013-06-14 00:09:52 +0000

    Prepping the pull request handler to extract the github payload.

--- prhandler.js
+++ prhandler.js
@@ -2,9 +2,16 @@
 
 module.exports = function(githubClient) {
     return function(req, res) {
-        console.log('Pull Request Hook recieved!');
-        console.log(req.body);
-        console.log('deferring action for now...');
+        var payload = JSON.parse(req.body.payload),
+            action = payload.action,
+            githubUser = payload.pull_request.user.login,
+            head = payload.pull_request.head,
+            base = payload.pull_request.base;
+        console.log('Received pull request "' + action + '" from ' + githubUser);
+        console.log('from:');
+        console.log(head);
+        console.log('to:');
+        console.log(base);
         res.end();
     };
 };
