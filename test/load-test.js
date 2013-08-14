cf1edcad3c79b448320c490d7be15ec15bd9973e
Matthew Taylor <mtaylor@numenta.com>
2013-08-13 21:32:42 -0700

    Adding a test that simply loads all modules for more accurate coverage reports.

---
+++ test/load-test.js
@@ -0,0 +1,5 @@
+// Load everything for code coverage
+var utils = require('../utils/general');
+utils.initializeModulesWithin('./handlers');
+utils.initializeModulesWithin('./validators');
+utils.initializeModulesWithin('./utils');
