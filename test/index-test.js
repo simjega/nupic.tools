f6918d4e20245c70d220e9146af71c509093a87b
Matthew Taylor <mtaylor@numenta.com>
2013-08-13 22:19:51 -0700

    Adding initial test for index.js

---
+++ test/index-test.js
@@ -0,0 +1,39 @@
+var assert = require('assert'),
+    proxyquire = require('proxyquire'),
+    connectStub = {},
+    utilStub,
+    configReaderStub,
+    githubHookSub;
+
+utilStub = {
+    sterilizeConfig: function(cfg) {
+        return cfg;
+    },
+    constructRepoClients: function() {}
+};
+configReaderStub = {
+    read: function() { return {host: 'host', port: 666}; }
+};
+
+describe('main index application', function() {
+    describe('when initially loaded', function() {
+        it('sends the right config file to configReader', function() {
+            proxyquire('./../index', {
+                './utils/general': utilStub,
+                './utils/configReader': {read: function(configPath) {
+                    assert.equal(configPath, './conf/config.json', 'Wrong default configuration path.');
+                    return {host: 'host', port: 666};
+                }},
+            });
+        });
+        it('constructs a proper github webhook url', function() {
+            utilStub.constructRepoClients = function(prWebhook, cfg) {
+                assert.equal(prWebhook, 'http://host:666/github-hook', 'Bad Github web hook url created');
+            };
+            proxyquire('./../index', {
+                './utils/general': utilStub,
+                './utils/configReader': configReaderStub
+            });
+        });
+    });
+});
