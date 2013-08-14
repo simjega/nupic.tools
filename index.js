de5c547ff8bf42ef80aa611da64bc1e20ecd6f6a
Matthew Taylor <mtaylor@numenta.com>
2013-08-13 21:44:38 -0700

    Extracting code from index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var assert = require('assert'),
     colors = require('colors'),
     // local libs
     utils = require('./utils/general'),
-    RepositoryClient = require('./utils/repoClient'),
     githubHookHandler = require('./githubHook'),
     // The configReader reads the given file, and merges it with any existing user
     // configuration file.
@@ -32,76 +31,13 @@ function die(err) {
     process.exit(-1);
 }
 
-/**
- * Given the entire merged app configuration, constructs a map of RepositoryClient
- * objects, properly registering Github webhook handlers for each if they don't
- * have them yet.
- *
- * If an error occurs during communication with Github, the application startup
- * will fail.
- * 
- * @param {Object} Application configuration.
- * @param {Function} Callback, to be sent a map of RepositoryClient objects, 
- * constructed using the "monitors" part of the configuration, keyed by Github 
- * "org/repo".
- */
-function constructRepoClients(config, callback) {
-    var repoClients = {},
-        count = 0;
-    // Set up one github client for each repo target in config.
-    Object.keys(config.monitors).forEach(function(monitorKey) {
-        var monitorConfig = config.monitors[monitorKey],
-            keyParts = monitorKey.split('/'),
-            org = keyParts.shift(),
-            repo = keyParts.shift(),
-            repoClient;
 
-        monitorConfig.organization = org;
-        monitorConfig.repository = repo;
-
-        repoClient = new RepositoryClient(monitorConfig);
-        console.log('RepositoryClient created for ' 
-            + monitorConfig.username.magenta + ' on ' 
-            + repoClient.toString().magenta);
-
-        repoClient.confirmWebhookExists(prWebhookUrl, ['pull_request', 'status'], function(err, hook) {
-            if (err) {
-                console.error(('Error during webhook confirmation for ' + repoClient.toString()).red);
-                die(err);
-            } else {
-                if (hook) {
-                    console.log(('Webhook created on ' + repoClient.toString() + ':\n'
-                                            + '\tfor "' + hook.events.join(', ') + '"\n'
-                                            + '\ton ' + hook.config.url).yellow);
-                } else {
-                    console.log(('Webhook exists for ' + repoClient.toString()).green);
-                }
-                count++;
-            }
-            repoClients[monitorKey] = repoClient;
-            if (count == (Object.keys(config.monitors).length))  {
-                callback(repoClients);
-            }
-        });
-    });
-}
-
-/* Removes the passwords from the config for logging. */
-function sterilizeConfig(config) {
-    var out = JSON.parse(JSON.stringify(config));
-    Object.keys(out.monitors).forEach(function(k) {
-        if (out.monitors[k].password) {
-            out.monitors[k].password = '<hidden>';
-        }
-    });
-    return out;
-}
 
 console.log('nupic.tools server starting...'.green);
 console.log('nupic.tools will use the following configuration:');
-console.log(JSON.stringify(sterilizeConfig(cfg), null, 2).yellow);
+console.log(JSON.stringify(utils.sterilizeConfig(cfg), null, 2).yellow);
 
-constructRepoClients(cfg, function(repoClients) {
+utils.constructRepoClients(prWebhookUrl, cfg, function(repoClients) {
     var dynamicHttpHandlerModules,
         // The Connect JS application
         app = connect();
--- utils/general.js
+++ utils/general.js
@@ -1,4 +1,5 @@
-var fs = require('fs');
+var fs = require('fs'),
+    RepositoryClient = require('./repoClient');
 
 /**
  * Reads all the JavaScript files within a directory, assuming they are all 
@@ -15,6 +16,74 @@ function initializeModulesWithin(dir) {
     return output;
 }
 
+/**
+ * Given the entire merged app configuration, constructs a map of RepositoryClient
+ * objects, properly registering Github webhook handlers for each if they don't
+ * have them yet.
+ *
+ * If an error occurs during communication with Github, the application startup
+ * will fail.
+ * 
+ * @param {String} Pull request web hook URL to register with Github.
+ * @param {Object} Application configuration.
+ * @param {Function} Callback, to be sent a map of RepositoryClient objects, 
+ * constructed using the "monitors" part of the configuration, keyed by Github 
+ * "org/repo".
+ */
+function constructRepoClients(prWebhookUrl, config, callback) {
+    var repoClients = {},
+        count = 0;
+    // Set up one github client for each repo target in config.
+    Object.keys(config.monitors).forEach(function(monitorKey) {
+        var monitorConfig = config.monitors[monitorKey],
+            keyParts = monitorKey.split('/'),
+            org = keyParts.shift(),
+            repo = keyParts.shift(),
+            repoClient;
+
+        monitorConfig.organization = org;
+        monitorConfig.repository = repo;
+
+        repoClient = new RepositoryClient(monitorConfig);
+        console.log('RepositoryClient created for ' 
+            + monitorConfig.username.magenta + ' on ' 
+            + repoClient.toString().magenta);
+
+        repoClient.confirmWebhookExists(prWebhookUrl, ['pull_request', 'status'], function(err, hook) {
+            if (err) {
+                console.error(('Error during webhook confirmation for ' + repoClient.toString()).red);
+                die(err);
+            } else {
+                if (hook) {
+                    console.log(('Webhook created on ' + repoClient.toString() + ':\n'
+                                            + '\tfor "' + hook.events.join(', ') + '"\n'
+                                            + '\ton ' + hook.config.url).yellow);
+                } else {
+                    console.log(('Webhook exists for ' + repoClient.toString()).green);
+                }
+                count++;
+            }
+            repoClients[monitorKey] = repoClient;
+            if (count == (Object.keys(config.monitors).length))  {
+                callback(repoClients);
+            }
+        });
+    });
+}
+
+/* Removes the passwords from the config for logging. */
+function sterilizeConfig(config) {
+    var out = JSON.parse(JSON.stringify(config));
+    Object.keys(out.monitors).forEach(function(k) {
+        if (out.monitors[k].password) {
+            out.monitors[k].password = '<hidden>';
+        }
+    });
+    return out;
+}
+
 module.exports = {
-    initializeModulesWithin: initializeModulesWithin
+    initializeModulesWithin: initializeModulesWithin,
+    constructRepoClients: constructRepoClients,
+    sterilizeConfig: sterilizeConfig
 };
