7fbdd2da4f19a2f199b18607ced87088287987ad
Matthew Taylor <mtaylor@numenta.com>
2013-09-25 11:14:56 -0700

    Passing contributor validation when user is same as API user.

--- validators/contributor.js
+++ validators/contributor.js
@@ -12,18 +12,23 @@ function isContributor(name, roster) {
 
 function validator(sha, githubUser, _, githubClient, callback) {
     console.log('Validating contributor "' + githubUser + '"...');
-    contributors.getAll(githubClient.contributorsUrl, function(err, contributors) {
-        var response = {
-            state: 'success',
-        };
-        if (err) return callback(err);
-        if (! isContributor(githubUser, contributors)) {
-            response.state = 'failure';
-            response.description = githubUser + ' has not signed the Numenta Contributor License';
-            response.target_url = 'http://numenta.org/licenses/cl/';
-        }
-        callback(null, response);
-    });
+    if (githubUser == githubUser.user) {
+        // The Github user assigned as the API client whould always pass validation.
+        callback(null, {state: 'success'})
+    } else {
+        contributors.getAll(githubClient.contributorsUrl, function(err, contributors) {
+            var response = {
+                state: 'success',
+            };
+            if (err) return callback(err);
+            if (! isContributor(githubUser, contributors)) {
+                response.state = 'failure';
+                response.description = githubUser + ' has not signed the Numenta Contributor License';
+                response.target_url = 'http://numenta.org/licenses/cl/';
+            }
+            callback(null, response);
+        });
+    }
 }
 
 module.exports.validate = validator;
