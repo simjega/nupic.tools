368f0a595ee5d09ac7ab69cc0737b8c70ba1fa6e
Matthew Taylor <matt@numenta.org>
2013-12-07 09:57:59 -0800

    Updated package and test

--- test/index-test.js
+++ test/index-test.js
@@ -15,10 +15,10 @@ configReaderStub = {
     read: function() { return {host: 'host', port: 666}; }
 };
 
-describe('main index application', function() {
+describe('main program', function() {
     describe('when initially loaded', function() {
         it('sends the right config file to configReader', function() {
-            proxyquire('./../index', {
+            proxyquire('./../program', {
                 './utils/general': utilStub,
                 './utils/configReader': {read: function(configPath) {
                     assert.equal(configPath, './conf/config.json', 'Wrong default configuration path.');
@@ -30,7 +30,7 @@ describe('main index application', function() {
             utilStub.constructRepoClients = function(prWebhook, cfg) {
                 assert.equal(prWebhook, 'http://host:666/github-hook', 'Bad Github web hook url created');
             };
-            proxyquire('./../index', {
+            proxyquire('./../program', {
                 './utils/general': utilStub,
                 './utils/configReader': configReaderStub
             });
