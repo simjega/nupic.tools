b642e159296738cf335c97283386d514326f2efc
Matthew Taylor <rhyolight@gmail.com>
2014-05-30 12:16:19 -0700

    Added logic to update nupic.regression on push to nupic master.

--- bin/post_nupic_push.sh
+++ bin/post_nupic_push.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
+## 
+# This script is to be executed by the tooling server every time there is a push
+# to nupic master branch. It is currently used to:
+# - generate nupic and nupic.core documentation
+# - update the sha in nupic.regression of the latest commit pushed
+#
 ## Assumptions
+#
 # - numenta/nupic is checked out somewhere within reach cloned from  
 #   git@github.com:numenta/nupic.git as `origin`.
 #   - $NUPIC points to the location of this checkout.
-# - numenta/nupic.core is checkout out somewhere within reach cloned from
+#
+# - numenta/nupic.core is checked out out somewhere within reach cloned from
 #   git@github.com:numenta/nupic.core.git as `origin`.
 #   - $NUPIC_CORE points to the location of this checkout.
+#
+# - numenta/nupic.regression is checked out out somewhere within reach cloned from
+#   git@github.com:numenta/nupic.regression.git as `origin`.
+#   - $NUPIC_REGRESSION points to the location of this checkout.
+#
 # - numenta/numenta.org repository is checked out from 
 #   https://github.com/numenta/numenta.org
 #   - $NUMENTA_ORG points to the location of this checkout
-
-echo
-echo "Building NuPIC API docs..."
-echo
 
 if [ -z "$NUPIC" ]; then
     echo "In order for this script to run properly, you must have the "
@@ -30,8 +39,18 @@ if [ -z "$NUMENTA_ORG" ]; then
     echo "\$NUMENTA_ORG environment variable set to the numenta/numenta.org checkout."
     exit 1
 fi
+if [ -z "$NUPIC_REGRESSION" ]; then
+    echo "In order for this script to run properly, you must have the "
+    echo "\$NUPIC_REGRESSION environment variable set to the numenta/nupic.regression checkout."
+    exit 1
+fi
 
 cwd=`pwd`
+
+echo
+echo "Building NuPIC API docs..."
+echo
+
 cd $NUPIC
 
 echo
@@ -65,6 +84,23 @@ git commit -m "NuPIC Doxygen automated doc build."
 git push origin gh-pages
 
 echo
-echo Done.
+echo "Done building and pushing new docs."
+echo
+
+echo
+echo "Updating nupic.regression with newest commit sha on master..."
+echo
+
+cd $NUPIC
+sha=`git log -1 --pretty=oneline | sed -E "s/^([^[:space:]]+).*/\1/" | tr -d ' '`
+cd $NUPIC_REGRESSION
+echo $sha > nupic_sha.txt
+git add nupic_sha.txt
+git commit -m "Automated update of nupic master sha to ${sha}."
+git push origin master
+
+echo 
+echo "Done updating nupic.regression."
+echo
 
 cd $cwd
