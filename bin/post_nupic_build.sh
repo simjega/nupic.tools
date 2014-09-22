02640b8669c312dc6a906fa91557fab523ee51be
Matthew Taylor <rhyolight@gmail.com>
2014-09-22 11:45:19 -0700

    Adding auto SHA update to nupic.research.

--- bin/post_nupic_build.sh
+++ bin/post_nupic_build.sh
@@ -25,16 +25,20 @@ if [ -z "$NUPIC_REGRESSION" ]; then
     echo "\$NUPIC_REGRESSION environment variable set to the numenta/nupic.regression checkout."
     exit 1
 fi
+if [ -z "$NUPIC_RESEARCH" ]; then
+    echo "In order for this script to run properly, you must have the "
+    echo "\$NUPIC_RESEARCH environment variable set to the numenta/nupic.research checkout."
+    exit 1
+fi
 
 cwd=`pwd`
-
-echo
-echo "Updating nupic.regression with newest commit sha on master..."
-echo
 
 cd $NUPIC
 git pull upstream master
 sha=`git log -1 --pretty=oneline | sed -E "s/^([^[:space:]]+).*/\1/" | tr -d ' '`
+echo
+echo "Updating nupic.regression with newest commit sha on master..."
+echo
 cd $NUPIC_REGRESSION
 git fetch upstream
 git merge upstream/master --no-edit
@@ -42,9 +46,22 @@ echo $sha > nupic_sha.txt
 git add nupic_sha.txt
 git commit -m "Automated update of nupic master sha to ${sha}."
 git push upstream master
-
 echo 
 echo "Done updating nupic.regression."
+echo
+
+echo
+echo "Updating nupic.research with newest commit sha on master..."
+echo
+cd $NUPIC_RESEARCH
+git fetch upstream
+git merge upstream/master --no-edit
+echo $sha > nupic_sha.txt
+git add nupic_sha.txt
+git commit -m "Automated update of nupic master sha to ${sha}."
+git push upstream master
+echo 
+echo "Done updating nupic.research."
 echo
 
 cd $cwd
